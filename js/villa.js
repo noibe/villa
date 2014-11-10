6f1ee0a4364c64fff298374ba32ff8f34c822ddb
Eduardo Barros <eduardo@noibe.com>
2014-11-09 23:33:24 -0200

    Add Mowe Font Add new license to Villa-grid.css, Villa.css and Villa.js

--- js/villa.js
+++ js/villa.js
@@ -1,7 +1,7 @@
 /*!
  * Villa Framework v2.0.0 (http://getvilla.org/)
  * Copyright 2013-2015 Noibe Developers
- * Licensed under MIT (/LICENSE)
+ * Licensed under MIT (https://github.com/noibe/villa/blob/master/LICENSE)
 */
 
 var colors;
