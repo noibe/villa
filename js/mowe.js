e538be9f4d978e60fa0dfa89bb9a46c4a48acc9c
Eduardo Barros <eduardo@noibe.com>
2014-12-23 12:24:23 -0200

    New Global Container and typography tests

--- js/mowe.js
+++ js/mowe.js
@@ -4,58 +4,46 @@
  * Licensed under MIT (https://github.com/noibe/villa/blob/master/LICENSE)
  */
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)){ window.isMobile = true; } else { window.isMobile = false; }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+	window.isMobile = true;
+} else {
+	window.isMobile = false;
+}
 
-if(window.innerWidth > 768) {
+if (window.innerWidth > 768) {
 	document.getElementsByClassName('hero')[0].style.height = (window.innerHeight - 60) + "px";
 } else {
 	document.getElementsByClassName('hero')[0].style.height = (window.innerHeight - 120) + "px";
 }
 
-window.onresize = function() {
-	if(window.innerWidth > 768) {
+window.onresize = function () {
+	if (window.innerWidth > 768) {
 		document.getElementsByClassName('hero')[0].style.height = (window.innerHeight - 60) + "px";
 	} else {
 		document.getElementsByClassName('hero')[0].style.height = (window.innerHeight - 120) + "px";
 	}
 };
 
-
-window.onscroll = function (event) {
-	if (!window.isMobile) {
-		if ((window.pageYOffset || doc.scrollTop) > (window.innerHeight - 64)) {
-			if (!$('.navbar').hasClass('white')) {
-				$('.navbar').addClass('white');
-			}
-		} else {
-			if ($('.navbar').hasClass('white')) {
-				$('.navbar').removeClass('white');
-			}
-		}
-	}
-};
-
-$('.wow').click(function() {
-	if(!$(this).hasClass('show')) {
+$('.wow').click(function () {
+	if (!$(this).hasClass('show')) {
 		$(this).addClass('show');
 	} else {
 		$(this).removeClass('show');
 	}
 });
 
-$('.navbar header .toggle').click(function() {
-	console.log($(this).closest('.navbar').find('.menu'));
+$('.navbar header .toggle').click(function () {
 	$(this).closest('.navbar').find('.menu').toggleClass('show');
 });
 
 $('.timeline .nav li').click(function () {
-	console.log($(this).closest('.timeline').find('.content li').get($(this).index()));
+	$(this).closest('.timeline').find('.content li').get($(this).index());
 	var index = $(this).index();
 	$($(this).closest('.timeline')).attr('class', 'timeline');
 	$($(this).closest('.timeline')).addClass('slide-' + index);
-})
+});
 
-window.onload = function() {
+window.onload = function () {
 
 	document.getElementById("js-scroll-services").onclick = function fun() {
 
@@ -116,9 +104,157 @@ window.onload = function() {
 	function ease(t, b, c, d) {
 		return Math.round(-c * Math.cos(t / d * (Math.PI / 2)) + c + b);
 	}
+
+	/* */
+	$('.block').scrollList({
+		beforePadding: 65
+	});
 };
+
+/* Mowe ScrollList v1.0.0 */
+
+(function ($) {
+
+	/* Constructor of scrollList functions */
+	$.fn.scrollList = function (options) {
+
+		var settings = $.extend({
+			beforePadding: 0,
+			startName: "",
+			target: "body"
+		}, options);
+
+		var addressListSettings = {
+			beforePadding: settings.beforePadding,
+			parent: settings.target,
+			startName: settings.startName
+		};
+
+		var addressList = $(this).getAddress(addressListSettings);
+		var element = this;
+		var pastActive;
+
+		$(window).resize(function () {
+			addressList = $(element).getAddress(addressListSettings);
+		});
+
+		$(window).scroll(function () {
+			var active = $(this).getScrollActiveItem(addressList);
+
+			if (active != pastActive) {
+				if (!$(settings.target).hasClass(active)) {
+
+					if ($(settings.target).hasClass(pastActive)) {
+						$(settings.target).removeClass(pastActive);
+					}
+
+					$(settings.target).addClass(active);
+
+					pastActive = active;
+				}
+			}
+		});
+	};
+
+	/* Get What Scroll Item is Active */
+	$.fn.getScrollActiveItem = function (addressList) {
+
+		var currentLocal;
+
+		for (i = 0; i < addressList.length; i++) {
+			if (addressList[i].address < $(window).scrollTop()) {
+				currentLocal = addressList[i].name;
+			}
+		}
+
+		if (currentLocal) {
+			return currentLocal;
+		} else {
+			return null;
+		}
+
+	};
+
+	/* Get Address List Function */
+	$.fn.getAddress = function (options) {
+
+		//beforePadding attr is nonfunctional
+		//parent attr is nonfunctional (at this version)
+		//startName attr is optional
+		var settings = $.extend({
+			addAddressAttr: true,
+			beforePadding: 0,
+			parent: "body",
+			startName: ""
+		}, options);
+
+		//Initializing the address list array
+		var addressList = new Array();
+
+		if (!settings.name) {
+			this.each(function (index) {
+
+				var name = "";
+
+				if (!settings.startName) {
+					//default
+					name = $(this).attr("id");
+				} else {
+					//when a startName attr was declared
+					name = settings.startName + "-" + (index + 1);
+				}
+
+				//fill the address list
+				addressList.push({
+					name: name,
+					address: this.offsetTop - settings.beforePadding
+				});
+
+				if (settings.addAddressAttr) {
+					$(this).attr("data-scroll-target", name);
+				}
+
+			});
+		}
+
+		return addressList;
+
+	};
+
+}(jQuery));
 
 /* external funcs */
 
+function getPosition(element) {
+	var xPosition = 0;
+	var yPosition = 0;
+
+	while (element) {
+		xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+		yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+		element = element.offsetParent;
+	}
+	return {x: xPosition, y: yPosition};
+}
+
+//get scroll position from top
+function getScrollTop() {
+	if (typeof pageYOffset != 'undefined') {
+		//most browsers except IE before #9
+		return pageYOffset;
+	}
+	else {
+		var B = document.body; //IE 'quirks'
+		var D = document.documentElement; //IE with doctype
+		D = (D.clientHeight) ? D : B;
+		return D.scrollTop;
+	}
+}
+
 //removes class with prefix
-jQuery.fn.removeClassLike = function(prefix){ var classes = this.attr("class").split(" ").filter(function(c) { return c.lastIndexOf(prefix, 0) !== 0; }); return this.attr("class", classes.join(" ")); }
+jQuery.fn.removeClassLike = function (prefix) {
+	var classes = this.attr("class").split(" ").filter(function (c) {
+		return c.lastIndexOf(prefix, 0) !== 0;
+	});
+	return this.attr("class", classes.join(" "));
+}
